c849dee5c601b33d294f35fc08834d4e1cb492b4
Temir <75352989+Temir2700@users.noreply.github.com>
2024-07-09 22:56:12 +0600

    upd

--- main.js
+++ main.js
@@ -39,3 +39,51 @@ document.addEventListener("DOMContentLoaded", function () {
 
   window.addEventListener("scroll", handleScroll);
 });
+
+// document.getElementById("menu-icon").addEventListener("click", function () {
+//
+// });
+
+const toggleBar = () => {
+  const navLinks = document.getElementById("nav-links");
+  navLinks.classList.toggle("active");
+  console.log("fsdfds");
+};
+
+document.getElementById("menuIcon").onclick = toggleBar;
+
+const showAlert = () => {
+  document.getElementById("customAlert").classList.add("open");
+};
+
+const closeAlert = () => {
+  console.log("here");
+  document.getElementById("customAlert").classList.remove("open");
+};
+
+// document.getElementById("closeAlertButton").onclick = closeAlert();
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contactForm");
+  const submitBtn = document.getElementById("submitBtn");
+  const requiredFields = form.querySelectorAll("input[required]");
+
+  requiredFields.forEach((field) => {
+    field.addEventListener("input", () => {
+      const allFilled = Array.from(requiredFields).every(
+        (input) => input.value.trim() !== ""
+      );
+      submitBtn.disabled = !allFilled;
+    });
+  });
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    showAlert();
+    // Add form submission logic here
+  });
+});
+
+document.getElementById("closeAlertButton").onclick = closeAlert;
